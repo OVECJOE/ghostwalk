fb2bac251e913648667c2ddd8bbdee52324f9770
OVECJOE <vohachor@gmail.com>
2024-11-22 13:35:29 +0100

    updated code to work with production build

--- client/src/services/authService.tsx
+++ client/src/services/authService.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from '../utils/constants';
 
-console.log(API_BASE_URL);
 export const createAccount = async ({ email, username, password }: signFormType) => {
     try {
         const { data } = await axios.post(`${API_BASE_URL}/auth/register`, {
